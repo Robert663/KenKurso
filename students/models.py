f578e120eff86606fd2ba15941ad38e2c5da1346
Lucas Bryan <lucasbryan0217@gmail.com>
2022-07-11 20:19:27 -0300

    feat: create models students

--- students/models.py
+++ students/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    active = models.BooleanField(default=True)
+
+    user_id = models.OneToOneField('users.User', on_delete=models.CASCADE, primary_key=True)
+    course_id = models.OneToOneField('courses.Course', on_delete=models.CASCADE, primary_key=True)
