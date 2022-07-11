af15cc138c921c31ebfd56129c08dec8bd29772b
Lucas Bryan <lucasbryan0217@gmail.com>
2022-07-11 20:47:43 -0300

    feat(Semesters-App): Crate a models Semester

--- semesters/models.py
+++ semesters/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Semester(models.Model):
+    semester = models.CharField(max_length=125)
+    course_id = models.ForeignKey('courses.Course', on_delete=models.CASCADE)
+    subjects_id = models.ManyToManyField('subjects.Subject', related_name='semester')
