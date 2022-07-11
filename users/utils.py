e4c793c8b776b017377da40ad2760727e123be73
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2022-07-11 20:30:48 -0300

    feat(users-app): users app models and util created

--- users/utils.py
+++ users/utils.py
@@ -1,1 +1,30 @@
-o
+from django.contrib.auth.models import BaseUserManager
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, email, password, is_professor, is_coordination, **extra_fields):
+        now = timezone.now()
+        if not email:
+            raise ValueError("email precisa ser passado")
+        
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            is_coordination=is_coordination,
+            is_professor=is_professor,
+            updated_at=now,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_user(self, email, password, **extra_fields):
+        return self._create_user(email, password,False, False, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True, **extra_fields)
+
