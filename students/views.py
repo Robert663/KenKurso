a5d5aff5aa1c2f6c4eca26889dda0ca7e5d97d8e
Omar <omarucolombari@gmail.com>
2022-07-12 16:29:38 -0300

    feat: created views for create teacher, student and coordenation

--- students/views.py
+++ students/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from courses.models import Course
+from semesters.models import Semester
+from students.serializers import StudentSerializer
+from users.serializers import UserSerializers
 
-# Create your views here.
+
+class CreateStudent(APIView):
+    def post(self, request):
+        course_id = request.data.pop('course')
+        course = Course.objects.get(id=course_id)
+
+        semester_id = request.data.pop('semester')
+        semester = Semester.objects.get(id=semester_id)
+
+        serializer_user = UserSerializers(data=request.data)
+        serializer_user.is_valid(raise_exception=True)
+        user = serializer_user.save()
+
+        student_data = {user, semester, course}
+        serializer_student = StudentSerializer(data=student_data)
+        serializer_student.is_valid(raise_exception=True)
+        serializer_student.save()
+        return Response(serializer_student.data, status=status.HTTP_201_CREATED)
--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from courses.models import Course
+from semesters.models import Semester
+from students.serializers import StudentSerializer
+from teachers.serializers import TeacherSerializers
+from users.serializers import UserSerializers
 
-# Create your views here.
+
+class CreateTeacher(APIView):
+    def post(self, request):
+        serializer_user = UserSerializers(data=request.data)
+        serializer_user.is_valid(raise_exception=True)
+        user = serializer_user.save(is_staff=True)
+
+        serializer_teacher = TeacherSerializers(data=user)
+        serializer_teacher.is_valid(raise_exception=True)
+        serializer_teacher.save()
+        return Response(serializer_teacher.data, status=status.HTTP_201_CREATED)
