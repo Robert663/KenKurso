5526a9b12b1427a03a0d4dbd9d63338e26aa31ff
Robert <lober3464@gmail.com>
2022-07-12 12:27:37 -0300

    feat(student-serializer): creating student serializer

---
+++ students/serializers.py
@@ -0,0 +1,23 @@
+
+
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
+from .models import Student
+
+
+class StudentSerializer(ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+        read_only_field = ['id']
+
+
+    def create(self, validated_data):
+        return Student.objects.create_user(**validated_data)
+
+class UpdateStudentSerializer(ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+        read_only_fields = ['id']
