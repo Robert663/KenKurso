a977c59d9fc9f68850a8c937a7b85d2bf25b9624
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2022-07-11 21:23:07 -0300

    feat(teachers-app): teacher models created

--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Teacher(models.Model):
+    user_id = models.OneToOneField(("users.User"), on_delete=models.CASCADE)
+    contrated_at = models.DateTimeField(datetime.now)
+
 # Create your models here.
