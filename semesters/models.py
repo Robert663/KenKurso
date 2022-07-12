ea75ef7baa1440cf26352fc08034f1ab6c836f0e
Robert <lober3464@gmail.com>
2022-07-12 10:35:23 -0300

    fix(hotfix): changing the semester field from char to integer

--- semesters/models.py
+++ semesters/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Semester(models.Model):
-    semester = models.CharField(max_length=125)
+    semester = models.IntegerField()
     course_id = models.ForeignKey('courses.Course', on_delete=models.CASCADE)
     subjects_id = models.ManyToManyField('subjects.Subject', related_name='semester')
