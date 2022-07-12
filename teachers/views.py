0322b4344a6a91f3e87b1f9da140f00eaa7f26b3
Omar <omarucolombari@gmail.com>
2022-07-12 15:24:00 -0300

    feat: created views for teachers

--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from teachers.models import Teacher
+from teachers.serializers import TeacherSerializers
 
-# Create your views here.
+
+class ListTeacher(generics.ListAPIView):
+    serializer_class = TeacherSerializers
+    queryset = Teacher.objects.all()
+
+
+class DetailsTeacher(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TeacherSerializers
+    queryset = Teacher.objects.all()
