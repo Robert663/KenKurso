17f65d2e54ac82492301810a207104c15fab004e
Robert <lober3464@gmail.com>
2022-07-12 12:18:08 -0300

    feat(course-serializer): creating course serializer

---
+++ courses/serializers.py
@@ -0,0 +1,22 @@
+from pkgutil import read_code
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
+from courses.models import Course
+
+
+class CourseSerializer(ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+        read_only_field = ['id', 'created_at']
+
+
+    def create(self, validated_data):
+        return Course.objects.create_user(**validated_data)
+
+class UpdateAccountSerializer(ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+        read_only_fields = ['id', 'created_at']
