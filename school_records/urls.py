c9d129949676ba84104420237337e5ef6fecceec
Carlos Vitor <limacarlosvitor@gmail.com>
2022-07-13 15:52:58 -0300

    feat(record-url): creating record urls

---
+++ school_records/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import views
+
+urlpatterns = [
+    path("records/", views.ListRecord.as_view()),
+    path("records/student/<int: student_id>/", views.ListRecordStudent.as_view()),
+    path("records/student/<int: student_id>/", views.CreateRecordStudent.as_view()),
+    path("records/subject/<int: subject_id>/", views.ListRecordSubject.as_view()),
+    path("records/<pk>/", views.DetailsRecord.as_view())
+]
