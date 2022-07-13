9dea7b3f3d68eb278b72c87f6107a43cc7b0b51e
Carlos Vitor <limacarlosvitor@gmail.com>
2022-07-13 18:34:20 -0300

    feat(student-url): creating student url

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('students/', views.CreateStudent.as_view()),
     path('students/', views.ListStudents.as_view()),
     path('students/<pk>/', views.RetrieveUpdateStudents.as_view()),
     path('students/course/<int:course_id>/', views.ListStudentCourseViews.as_view()),
