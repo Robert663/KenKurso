2fbb143b10e3735d977eb4484448655e7d294a0e
Carlos Vitor <limacarlosvitor@gmail.com>
2022-07-13 18:30:50 -0300

    feat(teacher-url): creating teacher url

---
+++ teachers/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('teacher/', views.CreateTeacher.as_view()),
+    path('teacher/', views.ListTeacher.as_view()),
+    path('teacher/<pk>/', views.DetailsTeacher.as_view()),
+]
