788565de9dfda98b621b9ac5dec340c1444abb53
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2022-07-13 18:46:10 -0300

    fix(semester.url): added semester/ to the urls in semester.url

--- semesters/urls.py
+++ semesters/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('course/<int:course_id>/', views.SemestersCourseView.as_view()),
-    path('<int:semester_id>/', views.SemesterView.as_view()),
+    path('semester/course/<int:course_id>/', views.SemestersCourseView.as_view()),
+    path('<semester/int:semester_id>/', views.SemesterView.as_view()),
 ]
