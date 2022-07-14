381bd241086fda5ecb78a3123fa46e9630925856
Robert <lober3464@gmail.com>
2022-07-14 07:34:18 -0300

    feat(subjects-url): adding the urls for subject

---
+++ subjects/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import views
+
+
+urlpatterns = [
+    path('subjects/', views.ListSubjectView.as_view()),
+    path('subjects/<int:num>/', views.ListSubjectView.as_view()),
+    path('subjects/teacher/<int:teacher_id>/', views.ListSubjectTeacherView.as_view()),
+    path('subjects/course/<int:course_id>/', views.ListSubjectCourseView.as_view()),
+    path('subjects/', views.CreateSubjectView.as_view()),
+    path('subjects/<pk>/', views.SubjectView.as_view()),
+]
