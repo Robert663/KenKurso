116d004b7e943025155dd8f1217e60c8c32fbddd
Robert <lober3464@gmail.com>
2022-07-14 14:03:39 -0300

    fix(users-serializers): fixing minors typos and correcting the login params
    ]

--- users/serializers.py
+++ users/serializers.py
@@ -6,9 +6,9 @@ class UserSerializers(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model: User
+        model = User
         fields = "__all__"
-        read_only_fields = ["id", "date_joined"]
+        read_only_fields = ["id", "date_joined", "updated_at"]
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
@@ -16,25 +16,23 @@ class UserSerializers(serializers.ModelSerializer):
 
 class SuperUserSerializers(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
     class Meta:
-        model: User
+        model = User
         fields = "__all__"
-        read_only_fields = ["id", "date_joined"]
+        read_only_fields = ["id", "date_joined", "updated_at"]
 
     def create(self, validated_data):
         return User.create_superuser(**validated_data)
 
 
-class LoginUserSerializers(serializers.ModelSerializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
+class LoginUserSerializers(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    password = serializers.CharField(write_only=True)
 
 
 class UpdateUserSerializers(serializers.ModelSerializer):
-
     class Meta:
-        model: User
-        fields: "__all__"
-        read_only_fields = ["id", 'date_joined']
+        model = User
+        fields = "__all__"
+        read_only_fields = ["id", 'date_joined', "updated_at"]
         extra_kwargs = {"password": {"write_only": True}}
