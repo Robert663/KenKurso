443f507826dd7e04cf9f41c849278d5db9e41e80
Robert <lober3464@gmail.com>
2022-07-14 11:26:06 -0300

    fix(semesters-urls): reallocating a symbol to the right spot

--- semesters/urls.py
+++ semesters/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('semester/course/<int:course_id>/', views.SemestersCourseView.as_view()),
-    path('<semester/int:semester_id>/', views.SemesterView.as_view()),
+    path('semester/<int:semester_id>/', views.SemesterView.as_view()),
 ]
