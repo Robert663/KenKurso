c0704439d0256b437dafbd5a543cb216870b6727
Robert <lober3464@gmail.com>
2022-07-14 11:23:44 -0300

    fix(courses-urls): fixing views import and url path

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import views
+from . import views
 
 urlpatterns = [
-    path("courses/", views.ListCourseView.as_view()),
+    path("courses/", views.ListCreateCourseView.as_view()),
     path("courses/<pk>/", views.DetailsCourseView.as_view()),
-    path("courses/students/<int: student_id>/", views.ListStudentCourse.as_view())
+    path("courses/students/<int:student_id>/", views.ListStudentCourse.as_view())
 ]
