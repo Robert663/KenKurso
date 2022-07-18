c1164ba7de61fd307f0f5da0521e0e2cad38ae72
Robert <lober3464@gmail.com>
2022-07-18 13:50:11 -0300

    fix(school_records-urls): adjusting urls

--- school_records/urls.py
+++ school_records/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("records/", views.ListRecord.as_view()),
-    path("records/student/<int:student_id>/", views.ListRecordStudent.as_view()),
+    path("records/students/<int:student_id>/", views.ListRecordStudent.as_view()),
     path("records/student/<int:student_id>/", views.CreateRecordStudent.as_view()),
     path("records/subject/<int:subject_id>/", views.ListRecordSubject.as_view()),
     path("records/<pk>/", views.DetailsRecord.as_view())
-]
+]   
