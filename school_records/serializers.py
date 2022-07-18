fb8225054369a173d1cf0df801bd6aeeb5b3df5a
Robert <lober3464@gmail.com>
2022-07-18 13:44:58 -0300

    fix(school_records-serializers): adjusting serializer to work properly

--- school_records/serializers.py
+++ school_records/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
+
+from students.serializers import StudentSerializer
 from .models import School_Record
 
 
 class SchoolRecordSerializers(serializers.ModelSerializer):
-
+    student = StudentSerializer(read_only=True)
     class Meta:
         model = School_Record
         fields = "__all__"
-        read_only_fields = ["id"]
