bb8a11ee5d850c5f128019c599bc407c0d5e6c72
Robert <lober3464@gmail.com>
2022-07-18 14:02:59 -0300

    fix(school_records-views): fixing CreateRecordStudent, ListRecordStudent and ListRecordSubject - views

--- school_records/views.py
+++ school_records/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 from rest_framework.views import APIView, Response, status, Request
+
+import school_records
 from .models import School_Record
 from .serializers import SchoolRecordSerializers
 from students.models import Student
@@ -24,7 +26,7 @@ class CreateRecordStudent(APIView):
         student = get_object_or_404(Student, pk=student_id)
         schoolRecordSerializer = SchoolRecordSerializers(data=request.data)
         schoolRecordSerializer.is_valid(raise_exception=True)
-        schoolRecordSerializer.save(student_id=student)
+        schoolRecordSerializer.save(student_id=student.user.id)
 
         return Response(schoolRecordSerializer.data, status.HTTP_201_CREATED)
 
@@ -34,7 +36,7 @@ class ListRecordStudent(ListAPIView):
     def get(self, request, student_id):
 
         student = School_Record.objects.filter(student_id = student_id ).all()
-        studentSerializer = SchoolRecordSerializers(student)
+        studentSerializer = SchoolRecordSerializers(student, many=True)
 
         return Response(studentSerializer.data)
 
@@ -44,6 +46,6 @@ class ListRecordSubject(APIView):
     def get(self, request, subject_id):
 
         subject = School_Record.objects.filter(subject_id = subject_id ).all()
-        subjectSerializer = SchoolRecordSerializers(subject)
+        subjectSerializer = SchoolRecordSerializers(subject, many=True)
 
         return Response(subjectSerializer.data)
