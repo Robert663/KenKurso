bcb65437da53fd1f0a410ebd4c3249f4bf2e896b
Robert <lober3464@gmail.com>
2022-07-18 14:04:44 -0300

    fix(student-serializer): adding the right fields to the studentSerializer

--- students/serializers.py
+++ students/serializers.py
@@ -1,26 +1,32 @@
-
-
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from courses import models
+from courses.serializers import CourseSerializer
+
 from semesters.serializers import StudentSemesterSerializer
 
 from users.serializers import UserSerializers
 
 from .models import Student
 
+
 class StudentDisplaySerializer(ModelSerializer):
     user = UserSerializers(read_only=True)
-    semester = StudentSemesterSerializer(read_only=True)
-    class Meta:
-        model = Student
-        fields = ["active","user","semester"]
-        depth=0
-class StudentSerializer(ModelSerializer):
 
     class Meta:
         model = Student
-        fields = '__all__' 
+        fields = ["active", "user", "semester"]
+        depth = 0
+
+
+class StudentSerializer(ModelSerializer):
+    user = UserSerializers(read_only=True)
+    course = CourseSerializer(read_only=True)
+    semester = StudentSemesterSerializer(read_only=True)
+
+    class Meta:
+        model = Student
+        fields = "__all__"
 
 
     def create(self, validated_data):
@@ -31,14 +37,14 @@ class StudentSerializer(ModelSerializer):
 class UpdateStudentSerializer(ModelSerializer):
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = "__all__"
 
 
 class DeactiveStudentSerializer(ModelSerializer):
     class Meta:
         model = Student
-        fields = ['active']
+        fields = ["active"]
 
     def update(self, instance, validated_data):
-        validated_data['active'] = False
+        validated_data["active"] = False
         return super().update(instance, validated_data)
