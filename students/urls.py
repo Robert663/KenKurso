0eb81bbb06670c8a7cbfa6e3d837b737ccaa5aa7
Robert <lober3464@gmail.com>
2022-07-18 14:05:58 -0300

    fix(student-urls): changing urls to make more sense

--- students/urls.py
+++ students/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('students/', views.CreateStudent.as_view()),
-    path('student/', views.ListStudents.as_view()),
+    path('student/', views.CreateStudent.as_view()),
+    path('students/', views.ListStudents.as_view()),
     path('students/<pk>/', views.RetrieveUpdateStudents.as_view()),
     path('students/course/<int:course_id>/', views.ListStudentCourseViews.as_view()),
     path('students/<pk>/deactivate/', views.DeactiveStudent.as_view()),
