61b9980782a47454654d882a2ee420d2fadef927
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2022-07-19 03:52:25 -0300

    heroku config
    Signed-off-by: oswaldo <91742210+oswaldo-deco@users.noreply.github.com>

--- KenKurso/settings.py
+++ KenKurso/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://kenkurso-api.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["kenkurso-api.herokuapp.com", "localhost"]
 
 
 # Application definition
