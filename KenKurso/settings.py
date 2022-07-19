341adf6babd312b08a798f9ed6ff546ccd4da417
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2022-07-19 03:37:16 -0300

    heroku config
    Signed-off-by: oswaldo <91742210+oswaldo-deco@users.noreply.github.com>

--- KenKurso/settings.py
+++ KenKurso/settings.py
@@ -111,13 +111,16 @@ else:
 
         
     }
+    
     DATABASE_URL = os.environ.get('DATABASE_URL')
     
     if DATABASE_URL:
         db_from_env = dj_database_url.config(
-        default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-    DATABASES_URL['default'].update(db_from_env)
+            default=DATABASE_URL, conn_max_age=500, ssl_require=True
+        )
+    DATABASES['default'].update(db_from_env)
     DEBUG = False
+
 
 
 
